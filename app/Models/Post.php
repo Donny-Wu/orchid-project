bc2de6d2ac50b127215b43110e7302bba922d393
DonnyWu <dong.yuen5@gmail.com>
2025-02-06 15:21:47 +0800

    feature:增修 過濾、排序資料案例

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,15 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Orchid\Screen\AsSource;
+use Orchid\Filters\Filterable;
+use Orchid\Attachment\Attachable;
+use Orchid\Attachment\Models\Attachment;
+use Orchid\Filters\Types\Like;
 
 class Post extends Model
 {
-    use HasFactory,AsSource;
+    use HasFactory,AsSource,Attachable, Filterable;
     protected $fillable = [
         'title',
         'description',
         'body',
         'author'
+    ];
+    protected $allowedSorts = [
+        'title',
+        'created_at',
+        'updated_at'
+    ];
+    protected $allowedFilters = [
+        'title' => Like::class,
     ];
 
 }
--- app/Orchid/Layouts/PostListLayout.php
+++ app/Orchid/Layouts/PostListLayout.php
@@ -6,6 +6,8 @@ use Orchid\Screen\Layouts\Table;
 use Orchid\Screen\TD;
 use App\Models\Post;
 use Orchid\Screen\Actions\Link;
+use Orchid\Screen\Fields\Input;
+
 
 
 class PostListLayout extends Table
@@ -28,11 +30,11 @@ class PostListLayout extends Table
     protected function columns(): iterable
     {
         return [
-            TD::make('title','Title')->render(function(Post $post){
+            TD::make('title','Title')->sort()->filter(Input::make())->render(function(Post $post){
                 return Link::make($post->title)->route('platform.post.edit',['post'=>$post]);
             }),
-            TD::make('created_at','Created'),
-            TD::make('updated_at','Last edit')
+            TD::make('created_at','Created')->sort(),
+            TD::make('updated_at','Last edit')->sort()
         ];
     }
 }
